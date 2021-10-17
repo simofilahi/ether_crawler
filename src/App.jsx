4bd7089120fdfbccf3ab2be38e5fd6ed60606fb4
simofilahi <mohamedfilahi222@gmail.com>
2021-10-17 11:20:15 +0100

    (setup): add style framework

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import IndexPage from "./containers/IndexPage";
+
+function App() {
+  return (
+    <div className="bg-yellow-400">
+      <IndexPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/IndexPage/index.jsx
@@ -0,0 +1,5 @@
+const IndexPage = () => {
+  return <div> Ether Crawler</div>;
+};
+
+export default IndexPage;
